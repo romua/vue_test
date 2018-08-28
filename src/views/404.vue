b8e3f55990f87835c01c5b3249a2931c60580b92
Roman <roman.chapkaylo@sombrainc.com>
2018-08-28 12:55:42 +0300

    page 404 not found

---
+++ src/views/404.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page-not-found__wrapper">
+    <h1>Page not found</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: '404',
+};
+</script>
+<style scoped lang="scss">
+  .page-not-found__wrapper {
+    height: 100%;
+    width: 100%;
+    margin: 40px;
+  }
+</style>
